b71b6782f3051e84a540ef5c6ff2b03949ee823d
OleksandrRusavsky <75322230+OleksandrRusavsky@users.noreply.github.com>
2022-02-28 13:09:36 +0200

    created core

--- src/components/MintComponent.vue
+++ src/components/MintComponent.vue
@@ -6,15 +6,11 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
-
 export default {
   methods: {
-    ...mapActions(["mintNFT"]),
     mint() {
       const userAddress = localStorage.getItem("account");
       if (userAddress) {
-        this.mintNFT(userAddress);
       }
     }
   }
