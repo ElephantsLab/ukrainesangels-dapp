fb8ea7ebdee30a6ca28c4aec26b94fa36def70ba
OleksandrRusavsky <75322230+OleksandrRusavsky@users.noreply.github.com>
2022-02-28 13:10:53 +0200

    lint error avoid

--- src/components/MintComponent.vue
+++ src/components/MintComponent.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <h2>Mint</h2>
-    <button v-on:click="mint">mint</button>
+<!--    <button v-on:click="mint">mint</button>-->
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    mint() {
-      const userAddress = localStorage.getItem("account");
-      if (userAddress) {
-      }
-    }
+    // mint() {
+      // const userAddress = localStorage.getItem("account");
+      // if (userAddress) {
+      // }
+    // }
   }
 }
 </script>
