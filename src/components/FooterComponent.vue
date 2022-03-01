cb347b1a932efeb4cff39d81ea81e8c96433fd08
ElenaPohrebniak <ElenaPohrebniak>
2022-03-01 20:43:05 +0200

    added block footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,8 +1,36 @@
 <template>
-    <div class="footer">
-        <footer>
-            <a class="logo" href="#"></a>
-            <div></div>
-        </footer>
-    </div>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-wrapper">
+                <div class="footer-block">
+                    <a class="logo" href="#"></a>
+                </div>
+                <div class="footer-block">
+                    <span class="footer-block-name">Navigation</span>
+                    <ul class="ul-flex-column">
+                        <li><a class="footer-link" href="">Our mission</a></li>
+                        <li><a class="footer-link" href="">Situation in Ukraine</a></li>
+                        <li><a class="footer-link" href="">Collection</a></li>
+                        <li><a class="footer-link" href="">Donation</a></li>
+                    </ul>
+                </div>
+                <div class="footer-block">
+                    <span class="footer-block-name">useful links</span>
+                    <ul class="ul-flex-column">
+                        <li><a class="footer-link" href="">If you need help</a></li>
+                        <li><a class="footer-link" href="">If you want help</a></li>
+                        <li><a class="footer-link" href="">Name 1</a></li>
+                    </ul>
+                </div>
+                <div class="footer-block">
+                    <span class="footer-block-name">Social links</span>
+                    <ul class="ul-social-list">
+                        <li><a class="twitter" href=""></a></li>
+                        <li><a class="telegram" href=""></a></li>
+                        <li><a class="facebook" href=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
