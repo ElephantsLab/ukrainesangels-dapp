83b7a452ed5e278bc8d0e66ab246c199440ccfaf
ElenaPohrebniak <ElenaPohrebniak>
2022-03-02 20:58:23 +0200

    fixed styles for hover

--- src/views/InfoPageOne.vue
+++ src/views/InfoPageOne.vue
@@ -5,18 +5,18 @@
   <div class="container">
     <div class="need__help">
       <div class="need__help__inner">
-        <div class="to__main">
-          <div v-on:click="toMain">
+        <a class="to__main" v-on:click="toMain">
+          <div >
             <i class="i-arrow-drop-left-line"></i>
           </div>
           <p class="title__nav">To main page</p>
-        </div>
+        </a>
         <p class="need__help_title">If you need help</p>
         <p class="need__help__disc">
           Elit nisl, maecenas hac sit ut. Blandit orci urna netus sit neque sit laoreet eget urna. Commodo massa eros, molestie fermentum posuere sed. Sit vel, ultrices sed est malesuada suspendisse. 
         </p>
           <div class="organization">
-            <div class="organization__block">
+            <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -27,8 +27,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+           <a href="" target="_blank" rel="nofollow"  class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -39,8 +39,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+           <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -51,8 +51,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+           <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -63,8 +63,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+           <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -75,8 +75,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+           <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -87,8 +87,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-                      <div class="organization__block">
+           </a>
+           <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -99,8 +99,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+           <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -111,8 +111,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+           <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -123,8 +123,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+           <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -135,7 +135,7 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
+           </a>
         </div>
       </div>
     </div>
--- src/views/InfoPageTwo.vue
+++ src/views/InfoPageTwo.vue
@@ -5,18 +5,18 @@
 <div class="container">
     <div class="need__help">
       <div class="need__help__inner">
-        <div class="to__main">
-          <div v-on:click="toMain">
+        <a class="to__main" v-on:click="toMain">
+          <div>
             <i class="i-arrow-drop-left-line"></i>
           </div>
           <p class="title__nav">To main page</p>
-        </div>
+        </a>
         <p class="need__help_title">If you want help</p>
         <p class="need__help__disc">
           Elit nisl, maecenas hac sit ut. Blandit orci urna netus sit neque sit laoreet eget urna. Commodo massa eros, molestie fermentum posuere sed. Sit vel, ultrices sed est malesuada suspendisse. 
         </p>
           <div class="organization">
-            <div class="organization__block">
+           <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -27,8 +27,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+                      <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -39,8 +39,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+                      <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -51,8 +51,8 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
+           </a>
+                      <a href="" target="_blank" rel="nofollow" class="organization__block">
               <div class="organization__logo"></div>
               <div class="organization__disc">
                 <p class="organization__disc__title">Name of organization</p>
@@ -63,79 +63,11 @@
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
-           </div>
-           <div class="organization__block">
-              <div class="organization__logo"></div>
-              <div class="organization__disc">
-                <p class="organization__disc__title">Name of organization</p>
-                <p class="organization__disc__about"
-                >Short description what this organization doing and what kind of help can it be. Maybe 3rd row
-                </p>
-              </div>
-              <div class="organization__link">
-                <i class="i-share-box-fill"></i>
-              </div>
-           </div>
-           <div class="organization__block">
-              <div class="organization__logo"></div>
-              <div class="organization__disc">
-                <p class="organization__disc__title">Name of organization</p>
-                <p class="organization__disc__about"
-                >Short description what this organization doing and what kind of help can it be. Maybe 3rd row
-                </p>
-              </div>
-              <div class="organization__link">
-                <i class="i-share-box-fill"></i>
-              </div>
-           </div>
-                      <div class="organization__block">
-              <div class="organization__logo"></div>
-              <div class="organization__disc">
-                <p class="organization__disc__title">Name of organization</p>
-                <p class="organization__disc__about"
-                >Short description what this organization doing and what kind of help can it be. Maybe 3rd row
-                </p>
-              </div>
-              <div class="organization__link">
-                <i class="i-share-box-fill"></i>
-              </div>
-           </div>
-           <div class="organization__block">
-              <div class="organization__logo"></div>
-              <div class="organization__disc">
-                <p class="organization__disc__title">Name of organization</p>
-                <p class="organization__disc__about"
-                >Short description what this organization doing and what kind of help can it be. Maybe 3rd row
-                </p>
-              </div>
-              <div class="organization__link">
-                <i class="i-share-box-fill"></i>
-              </div>
-           </div>
-           <div class="organization__block">
-              <div class="organization__logo"></div>
-              <div class="organization__disc">
-                <p class="organization__disc__title">Name of organization</p>
-                <p class="organization__disc__about"
-                >Short description what this organization doing and what kind of help can it be. Maybe 3rd row
-                </p>
-              </div>
-              <div class="organization__link">
-                <i class="i-share-box-fill"></i>
-              </div>
-           </div>
-           <div class="organization__block">
-              <div class="organization__logo"></div>
-              <div class="organization__disc">
-                <p class="organization__disc__title">Name of organization</p>
-                <p class="organization__disc__about"
-                >Short description what this organization doing and what kind of help can it be. Maybe 3rd row
-                </p>
-              </div>
-              <div class="organization__link">
-                <i class="i-share-box-fill"></i>
-              </div>
-           </div>
+           </a>
+
+
+
+
         </div>
       </div>
       <div class="want__help">
