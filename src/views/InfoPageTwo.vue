77adc6d8706c6797258b98d6c017827f13c6ebe2
OleksandrRusavsky <75322230+OleksandrRusavsky@users.noreply.github.com>
2022-03-03 13:22:15 +0200

    connected donate links

--- src/views/InfoPageTwo.vue
+++ src/views/InfoPageTwo.vue
@@ -16,58 +16,17 @@
           Elit nisl, maecenas hac sit ut. Blandit orci urna netus sit neque sit laoreet eget urna. Commodo massa eros, molestie fermentum posuere sed. Sit vel, ultrices sed est malesuada suspendisse. 
         </p>
           <div class="organization">
-           <a href="" target="_blank" rel="nofollow" class="organization__block">
-              <div class="organization__logo"></div>
+           <a v-for="link in links" v-bind:key="link" v-bind:href="link[3]" target="_blank" rel="nofollow" class="organization__block">
+              <div class="organization__logo" v-bind:style="{ 'background-image': parseUrl(link[1]) }"></div>
               <div class="organization__disc">
-                <p class="organization__disc__title">Name of organization</p>
+                <p class="organization__disc__title">{{ link[0] }}</p>
                 <p class="organization__disc__about"
-                >Short description what this organization doing and what kind of help can it be. Maybe 3rd row
-                </p>
+                >{{ link[2] }}</p>
               </div>
               <div class="organization__link">
                 <i class="i-share-box-fill"></i>
               </div>
            </a>
-                      <a href="" target="_blank" rel="nofollow" class="organization__block">
-              <div class="organization__logo"></div>
-              <div class="organization__disc">
-                <p class="organization__disc__title">Name of organization</p>
-                <p class="organization__disc__about"
-                >Short description what this organization doing and what kind of help can it be. Maybe 3rd row
-                </p>
-              </div>
-              <div class="organization__link">
-                <i class="i-share-box-fill"></i>
-              </div>
-           </a>
-                      <a href="" target="_blank" rel="nofollow" class="organization__block">
-              <div class="organization__logo"></div>
-              <div class="organization__disc">
-                <p class="organization__disc__title">Name of organization</p>
-                <p class="organization__disc__about"
-                >Short description what this organization doing and what kind of help can it be. Maybe 3rd row
-                </p>
-              </div>
-              <div class="organization__link">
-                <i class="i-share-box-fill"></i>
-              </div>
-           </a>
-                      <a href="" target="_blank" rel="nofollow" class="organization__block">
-              <div class="organization__logo"></div>
-              <div class="organization__disc">
-                <p class="organization__disc__title">Name of organization</p>
-                <p class="organization__disc__about"
-                >Short description what this organization doing and what kind of help can it be. Maybe 3rd row
-                </p>
-              </div>
-              <div class="organization__link">
-                <i class="i-share-box-fill"></i>
-              </div>
-           </a>
-
-
-
-
         </div>
       </div>
       <div class="want__help">
@@ -107,6 +66,7 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 import HeaderComponent from "@/components/HeaderComponent";
 
 export default {
@@ -117,9 +77,11 @@ export default {
     return {
       donationAmount: 0.1,
       isMaxVal: false,
+      links: []
     }
   },
   methods: {
+    ...mapActions(["fetchHelpCenters"]),
     toMain() {
       this.$router.push("/");
     },
@@ -139,7 +101,18 @@ export default {
           this.isMaxVal = true;
         }
       }
+    },
+    parseLinks(data) {
+      if (data.length) {
+        this.links = data;
+      }
+    },
+    parseUrl(url) {
+      return `url('${url}')`;
     }
+  },
+  async mounted() {
+    this.parseLinks(await this.fetchHelpCenters("Donate"));
   }
 }
 </script>
