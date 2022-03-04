f9d18a8feff94f0bd83cb7dc026fa0f8b36d2517
ElenaPohrebniak <ElenaPohrebniak>
2022-03-04 13:25:39 +0200

    fixed links

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,23 +3,23 @@
         <div class="container">
             <div class="footer-wrapper">
                 <div class="footer-block">
-                    <a class="logo" href="#"></a>
+                    <a v-on:click="this.$router.push('/')" class="header-logo logo"></a>
                 </div>
                 <div class="footer-block">
                     <span class="footer-block-name">Navigation</span>
                     <ul class="ul-flex-column">
-                        <li><a class="footer-link" href="#our-mission">Our mission</a></li>
-                        <li><a class="footer-link" href="#situation">Situation in Ukraine</a></li>
-                        <li><a class="footer-link" href="#angel-collection">Collection</a></li>
-                        <li><a class="footer-link" href="#your-donate">Donation</a></li>
-                        <li><a class="footer-link" href="#our-team">Our Team</a></li>
+                        <li><a class="footer-link" v-on:click="this.$router.push('/')"  href="#our-mission">Our mission</a></li>
+                        <li><a class="footer-link" v-on:click="this.$router.push('/')"  href="#situation">Situation in Ukraine</a></li>
+                        <li><a class="footer-link" v-on:click="this.$router.push('/')"  href="#angel-collection">Collection</a></li>
+                        <li><a class="footer-link" v-on:click="this.$router.push('/')"  href="#your-donate">Donation</a></li>
+                        <li><a class="footer-link" v-on:click="this.$router.push('/')" href="#need-help">Help desc</a></li>
                     </ul>
                 </div>
                 <div class="footer-block">
                     <span class="footer-block-name">useful links</span>
                     <ul class="ul-flex-column">
-                        <li><a class="footer-link" href="#need-help">If you need help</a></li>
-                        <li><a class="footer-link" href="#need-help">If you want help</a></li>
+                        <li><a class="footer-link" v-on:click="this.$router.push('/')" href="https://ukrainesangels.com/InfoCenter">If you need help</a></li>
+                        <li><a class="footer-link" v-on:click="this.$router.push('/')" href="https://ukrainesangels.com/Donate">If you want help</a></li>
                     </ul>
                 </div>
                 <div class="footer-block">
