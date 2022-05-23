21d3f8bd3a62524de3d2d60e711f6e13f3f9bf19
Valentin <valentin@elephantslab.com>
2022-05-23 16:29:22 +0300

    removed active class from routing

--- src/router/index.js
+++ src/router/index.js
@@ -59,8 +59,6 @@ export const routes = [
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
-    linkActiveClass: "active",
-    linkExactActiveClass: "active",
 });
 
 export default router;
